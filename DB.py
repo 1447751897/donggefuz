31a5bd2f662efc4d1b68e219a5e3451bdd64cdf8
1447751897@qq.com <1447751897@qq.com>
2022-10-10 20:40:49 +0800

    数据库更改

--- DB.py
+++ DB.py
@@ -1,48 +1,31 @@
 import pymysql
 import random
+import random_test
+import random_2
 
-# 打开数据库连接
 conn=pymysql.connect(host='localhost',port=3306,user='root',password='123456',charset='utf8',db='student')
-# 使用 cursor() 方法创建一个游标对象 cursor
+
 cursor = conn.cursor()
 
-#cursor.execute("create database student")
-#conn.commit()
-
-#cursor.execute("show databases")
-# 使用 fetchone() 方法获取单条数据.
-#result = cursor.fetchall()
-#print(result)
-#sql1 = """drop table if exists class1"""
-#cursor.execute(sql1)
-#cursor.execute("use student")
 sql1 = """drop table if exists class1"""
 sql2="""
 create table class1(
-    que int not null auto_increment primary key,
-    id int,
+    id int not null auto_increment primary key,
+    que int,
     attendance varchar(20),
-    sampling varchar(20),
-    cnt int
+    score float
 )
 """
 cursor.execute(sql1)
 cursor.execute(sql2)
 conn.commit()
-#rank int auto_increment primary key,
+
 ran = random.sample(range(1, 91), 90)
 
 for i in ran:
-    sql3 = "insert into class1(id) values ('%s')" %(i)
+    sql3 = "insert into class1(que,attendance,score) values ('%d','%s','%f')" % (i,situation[i-1],x[i-1])
     cursor.execute(sql3)
     conn.commit()
 
-
-# 关闭数据库连接
-#cursor.execute("drop table class1")
-#conn.commit()
-
-#cursor.execute("drop database student")
-#conn.commit()
 cursor.close()
 conn.close()
